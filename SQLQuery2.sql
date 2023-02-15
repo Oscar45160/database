caadb6bb505187f3190c5c076d44b1a186bf833f
Oscar PIMPAUD <oscar.pimpaud@e-cdp.com>
2023-02-15 11:27:40 +0100

    initial commit

---
+++ SQLQuery2.sql
@@ -0,0 +1,21 @@
+CREATE TABLE ODS.dbo.BNLC (
+	id_lieu VARCHAR(MAX),
+	id_local VARCHAR(MAX),
+	nom_lieu VARCHAR(MAX),
+	ad_lieu VARCHAR(MAX),
+	com_lieu VARCHAR(MAX),
+	insee VARCHAR(MAX),
+	type_place VARCHAR(MAX),
+	date_maj VARCHAR(MAX),
+	ouvert VARCHAR(MAX),
+	source VARCHAR(MAX),
+	Xlong VARCHAR(MAX),
+	Ylat VARCHAR(MAX),
+	nbre_pl VARCHAR(MAX),
+	nbre_pmr VARCHAR(MAX),
+	duree VARCHAR(MAX),
+	horaire VARCHAR(MAX),
+	proprio VARCHAR(MAX),
+	lumiere VARCHAR(MAX),
+	comm VARCHAR(MAX)
+);
